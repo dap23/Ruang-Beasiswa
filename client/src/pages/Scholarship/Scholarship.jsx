3158f5cf6b07fbad9111b9eb1a103202d3447fd2
dap23 <daffa230601@gmail.com>
2022-07-21 20:00:48 +0700

    Edit

--- client/src/pages/Scholarship/Scholarship.jsx
+++ client/src/pages/Scholarship/Scholarship.jsx
@@ -7,7 +7,11 @@ import { useEffect } from "react";
 import axios from "axios";
 
 const Scholarship = () => {
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState({
+    show: false,
+    type: null,
+    id: null,
+  });
   const [msg, setMsg] = useState(null);
   const [showDel, setShowDel] = useState(false);
   const [data, setData] = useState({
@@ -19,10 +23,13 @@ const Scholarship = () => {
     description: "",
     kategori: "",
   });
+  const [editData, setEditData] = useState([]);
+  console.log(editData);
 
   const beasiswas = useStore((state) => state.beasiswas);
   console.log(beasiswas);
   const removeBeasiswa = useStore((state) => state.removeBeasiswa);
+  const editBeasiswa = useStore((state) => state.editBeasiswa);
   const setBeasiswa = useStore((state) => state.setBeasiswa);
 
   useEffect(() => {
@@ -76,6 +83,59 @@ const Scholarship = () => {
     }
   };
 
+  const handleAction = async (id, type) => {
+    switch (type) {
+      case "ADD":
+        setModal({
+          show: true,
+          type: "ADD",
+        });
+        break;
+      case "EDIT":
+        setModal({
+          show: true,
+          type: "EDIT",
+          id: id,
+        });
+        const res = await axios.get(`/scholarships/id/${id}`);
+        setEditData(res.data.data);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleChangeEdit = (e) => {
+    const value = e.target.value;
+    setEditData({ ...editData, [e.target.name]: value });
+  };
+
+  const handleSubmitEdit = async (e) => {
+    e.preventDefault();
+    const beasiswa = {
+      name: editData?.name,
+      image: editData?.image,
+      jenjang: editData?.jenjang,
+      kota: editData?.kota,
+      link: editData?.link,
+      description: editData?.description,
+      kategori: editData?.kategori,
+    };
+    try {
+      await axios({
+        method: "PUT",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        url: `/scholarships/${modal.id}`,
+        data: beasiswa,
+        withCredentials: true,
+      });
+      setMsg("Scholarship success edited!");
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       {msg ? (
@@ -93,14 +153,36 @@ const Scholarship = () => {
         ""
       )}
       <div className={styles.top}>
-        <h3>{modal ? "Add Beasiswa" : "Beasiswa"}</h3>
-        {modal ? (
-          <button onClick={() => setModal(false)}>Cancel</button>
+        <h3>
+          {modal.type === "ADD"
+            ? "Add Beasiswa"
+            : modal.type === "EDIT"
+            ? "Edit Beasiswa"
+            : "Beasiswa"}
+        </h3>
+        {modal.type === "ADD" || modal.type === "EDIT" ? (
+          <button
+            onClick={() =>
+              setModal({
+                show: false,
+                type: null,
+              })
+            }>
+            Cancel
+          </button>
         ) : (
-          <button onClick={() => setModal(true)}>Add Beasiswa</button>
+          <button
+            onClick={() =>
+              setModal({
+                show: true,
+                type: "ADD",
+              })
+            }>
+            Add Beasiswa
+          </button>
         )}
       </div>
-      {modal ? (
+      {modal.show && modal.type === "ADD" ? (
         <div className={styles.form}>
           <div className={styles.inputGroup}>
             <label htmlFor="Name">Name</label>
@@ -174,6 +256,87 @@ const Scholarship = () => {
             <button onClick={handleSubmit}>Submit</button>
           </div>
         </div>
+      ) : modal.show && modal.type === "EDIT" ? (
+        <div className={styles.form}>
+          <div className={styles.inputGroup}>
+            <label htmlFor="Name">Name</label>
+            <input
+              type="text"
+              placeholder="Name..."
+              id="Name"
+              name="name"
+              onChange={handleChangeEdit}
+              value={editData?.name}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="Image">Image</label>
+            <input
+              type="text"
+              placeholder="Image..."
+              id="Image"
+              name="image"
+              onChange={handleChangeEdit}
+              value={editData?.image}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="Jenjang">Jenjang</label>
+            <input
+              type="text"
+              placeholder="Jenjang..."
+              id="Jenjang"
+              name="jenjang"
+              onChange={handleChangeEdit}
+              value={editData?.jenjang}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="Domisili">Domisili</label>
+            <input
+              type="text"
+              placeholder="Domisili..."
+              id="Domisili"
+              name="kota"
+              onChange={handleChangeEdit}
+              value={editData?.kota}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="Kategori">Kategori</label>
+            <input
+              type="text"
+              placeholder="Kategori..."
+              id="Kategori"
+              name="kategori"
+              onChange={handleChangeEdit}
+              value={editData?.kategori}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="Link">Link Pendaftaran</label>
+            <input
+              type="text"
+              placeholder="Link Pendaftaran..."
+              id="Link"
+              name="link"
+              onChange={handleChangeEdit}
+              value={editData?.link}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <label htmlFor="Desc">Description</label>
+            <textarea
+              name="description"
+              id="desc"
+              placeholder="Description..."
+              onChange={handleChangeEdit}
+              value={editData?.description}></textarea>
+          </div>
+          <div className={styles.inputGroup}>
+            <button onClick={handleSubmitEdit}>Edit</button>
+          </div>
+        </div>
       ) : (
         <div className={styles.content}>
           <table>
@@ -202,7 +365,10 @@ const Scholarship = () => {
                       <td>{beasiswa?.jenjang}</td>
                       <td>
                         <div className={styles.button}>
-                          <AiFillEdit className={styles.edit} />
+                          <AiFillEdit
+                            className={styles.edit}
+                            onClick={() => handleAction(beasiswa?.id, "EDIT")}
+                          />
                           <AiFillDelete
                             className={styles.delete}
                             onClick={() => handleDelete(beasiswa?.id)}
--- client/src/store/slices/beasiswa.js
+++ client/src/store/slices/beasiswa.js
@@ -12,6 +12,27 @@ const beasiswa = (set, get) => ({
       beasiswas: state.beasiswas.filter((bea) => bea.id !== id),
     }));
   },
+  editBeasiswa: async (id, newBeasiswa) => {
+    await axios.put(`scholarships/${id}`);
+    set((state) => ({
+      beasiswas: state.beasiswas.map((beasiswa) => {
+        if (beasiswa.id === newBeasiswa.id) {
+          return {
+            ...beasiswa,
+            name: newBeasiswa.name,
+            image: newBeasiswa.image,
+            jenjang: newBeasiswa.jenjang,
+            kota: newBeasiswa.kota,
+            link: newBeasiswa.link,
+            description: newBeasiswa.description,
+            kategori: newBeasiswa.kategori,
+          };
+        } else {
+          return beasiswa;
+        }
+      }),
+    }));
+  },
 });
 
 export default beasiswa;
